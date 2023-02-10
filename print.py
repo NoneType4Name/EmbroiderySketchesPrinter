2f33d725376222641e1fe42a8b7988e520c7c0b8
AlexKim0710 <90710372+NoneType4Name@users.noreply.github.com>
2023-02-10 23:10:36 +0300

    update rint technology.
    Sincronyze MM and pixels.

--- print.py
+++ print.py
@@ -1,59 +1,59 @@
-# import win32print
-# import win32ui
-# from PIL import Image, ImageWin
+import win32print
+import win32ui
+from PIL import Image, ImageWin, ImageDraw
+
 #
-# #
-# # Constants for GetDeviceCaps
-# #
-# #
-# # HORZRES / VERTRES = printable area
-# #
-# HORZRES = 8
-# VERTRES = 10
-# #
-# # LOGPIXELS = dots per inch
-# #
-# LOGPIXELSX = 88
-# LOGPIXELSY = 90
-# #
-# # PHYSICALWIDTH/HEIGHT = total area
-# #
-# PHYSICALWIDTH = 110
-# PHYSICALHEIGHT = 111
+# Constants for GetDeviceCaps
 #
-# #
-# # PHYSICALOFFSETX/Y = left / top margin
-# #
-# PHYSICALOFFSETX = 112
-# PHYSICALOFFSETY = 113
-# file_name = "graph.png"
-# hDC = win32ui.CreateDC()
-# hDC.CreatePrinterDC(win32print.GetDefaultPrinter())
-# printable_area = hDC.GetDeviceCaps(HORZRES), hDC.GetDeviceCaps(VERTRES)
-# printer_size = hDC.GetDeviceCaps(PHYSICALWIDTH), hDC.GetDeviceCaps(PHYSICALHEIGHT)
-# printer_margins = hDC.GetDeviceCaps(PHYSICALOFFSETX), hDC.GetDeviceCaps(PHYSICALOFFSETY)
-# bmp = Image.open(file_name)
-# if bmp.size[0] > bmp.size[1]:
-#     bmp = bmp.rotate(90)
 #
-# ratios = [printer_size[0] / bmp.size[0], printer_size[1] / bmp.size[1]]
-# scale = min(ratios)
-# hDC.StartDoc(file_name)
-# hDC.StartPage()
+# HORZRES / VERTRES = printable area
 #
-# dib = ImageWin.Dib(bmp)
-# scaled_width, scaled_height = [int(scale * i) for i in bmp.size]
-# x1 = round((printer_size[0] - scaled_width) / 2)
-# y1 = round((printer_size[1] - scaled_height) / 2)
-# x2 = x1 + scaled_width
-# y2 = y1 + scaled_height
-# dib.draw(hDC.GetHandleOutput(), (x1, y1, x2, y2))
+HORZRES = 8
+VERTRES = 10
 #
-# hDC.EndPage()
-# hDC.EndDoc()
-# hDC.DeleteDC()
-from PIL import Image, ImageDraw
-image = Image.new("L", (1000, 1000), 255)
+# LOGPIXELS = dots per inch
+#
+LOGPIXELSX = 88
+LOGPIXELSY = 90
+#
+# PHYSICALWIDTH/HEIGHT = total area
+#
+PHYSICALWIDTH = 110
+PHYSICALHEIGHT = 111
+
+#
+# PHYSICALOFFSETX/Y = left / top margin
+#
+PHYSICALOFFSETX = 112
+PHYSICALOFFSETY = 113
+
+hDC = win32ui.CreateDC()
+hDC.CreatePrinterDC(win32print.GetDefaultPrinter())
+printable_area = hDC.GetDeviceCaps(HORZRES), hDC.GetDeviceCaps(VERTRES)
+printer_size = hDC.GetDeviceCaps(PHYSICALWIDTH), hDC.GetDeviceCaps(PHYSICALHEIGHT)
+DPI = hDC.GetDeviceCaps(88)
+
+
+def MillimetersToPixels(mm: int) -> int:
+    return round((mm * DPI) / 25.4)
+
+
+def DrawCircle(img, x, y, r, fill=None, outline=None, width=1):
+    img.ellipse((x-r, y-r, x+r, y+r), fill, outline, width)
+
+
+image = Image.new("L", printable_area, 255)
 drawImage = ImageDraw.Draw(image)
-drawImage.arc((0, 0, 100, 100), 30, 180, 0)
-image.show()
+drawImage.rectangle((0,0,*image.size), outline=0, width=MillimetersToPixels(1))
+DrawCircle(drawImage, MillimetersToPixels(100), MillimetersToPixels(100), MillimetersToPixels(10), outline=0, width=10)
+drawImage.point((MillimetersToPixels(100), MillimetersToPixels(100)))
+
+image = image.rotate(90 if image.size[0] > image.size[1] else 0)
+hDC.StartDoc('Graphic print.')
+hDC.StartPage()
+
+ImageWin.Dib(image).draw(hDC.GetHandleOutput(), (0, 0, *printable_area))
+
+hDC.EndPage()
+hDC.EndDoc()
+hDC.DeleteDC()
